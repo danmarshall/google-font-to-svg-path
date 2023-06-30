72bc26686a20855278980a46392a2319b5ab1859
Dan Marshall <danmar@microsoft.com>
2023-06-30 10:36:20 -0700

    build

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 ///<reference path="node_modules/makerjs/index.d.ts" />
-////<reference path="node_modules/@jscad/stl-serializer/index.d.ts" />
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
